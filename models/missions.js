19e73d1ace59fa65e7a9a3cbfb90e744291b9223
PETER MUYOMBA <111797748+pmuyomba11@users.noreply.github.com>
2023-09-14 16:46:28 -0400

    views and models update

---
+++ models/missions.js
@@ -0,0 +1,40 @@
+
+const marsMissions = [
+    {
+      name: "Curiosity",
+      launchDate: "26 Nov 2011",
+      operator: "NASA",
+      missionType: "Rover",
+      img: "https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/pia19808-main_tight_crop-monday.jpg",
+    },
+    {
+      name: "Opportunity",
+      launchDate: "8 Jul 2003",
+      operator: "NASA",
+      missionType: "Rover",
+      img: "https://media.istockphoto.com/id/1324502851/photo/rover-on-mars-surface-exploration-of-red-planet-space-station-expedition-perseverance.jpg?s=612x612&w=0&k=20&c=4kTI0OwxEqCt6o39DvDu86XAdJP3bwCakkhxaJO92zs=",
+    },
+    {
+      name: "Spirit",
+      launchDate: "10 Jun 2003",
+      operator: "NASA",
+      missionType: "Rover",
+      img: "https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/images/287330main_image_1215_full.jpg",
+    },
+    {
+      name: "Sojourner",
+      launchDate: "4 Dec 1996",
+      operator: "NASA",
+      missionType: "Rover",
+      img: "https://media.istockphoto.com/id/586703688/photo/crew-exploration-vehicle-docking.jpg?s=612x612&w=0&k=20&c=6C-cv_XtqNjr7XVw0RbA3ktO1YPyFMeTST8ZgpUxJx0=",
+    },
+    {
+      name: "Rosetta",
+      launchDate: "2 Mar 2004",
+      operator: "ESA",
+      missionType: "Gravity Assist",
+      img: "https://www.jpl.nasa.gov/spaceimages/images/largesize/PIA18891_hires.jpg",
+    },
+  ]
+
+  module.exports = marsMissions
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const colors = require('colors');
 const morgan = require('morgan');
+const missions = require('./models/missions');
 const PORT = process.env.PORT;
 //Initialization....
 const app = express();
@@ -11,8 +12,20 @@ const app = express();
 app.use(morgan('dev'));
 
 //Routes....
+//Index route
+app.get('/missions', (req, res) => {
+    res.render('index.ejs', {
+        allMissions: missions
+    })
+})
 
 
+//Show route...
+app.get('/missions/:id', (req, res) => {
+    res.render('show.ejs', {
+        mission: missions[req.params.id]
+    })
+})
 //Listening...
 app.listen(PORT, () => {
     console.log('====================================');
